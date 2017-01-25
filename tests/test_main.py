2d6058c3214dd288d69a023c337c48ff8821a5d1
Noi Sek <noi.t.sek@gmail.com>
2017-01-25 03:20:32 -0800

    First useful tests.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,25 @@
+import pytest
+
 from flask_microservices import (
-  MicroServicesApp,
-  MicroServicesLoader,
-  Router,
-  url,
-  exceptions
+    MicroServicesApp,
+    MicroServicesLoader,
+    Router,
+    url,
+    exceptions
 )
 
-def test(x):
-  return True
 
-def test_answer():
-  assert test(True) == True
+@pytest.fixture
+def app():
+    return MicroServicesApp("testapp")
+
+
+def test_app_initializes_successfully(app):
+    # Should be named correctly
+    assert app.import_name == "testapp"
+
+    # Should have a MicroServicesLoader instance
+    assert isinstance(app.jinja_options['loader'], MicroServicesLoader) == True
+
+    # Should have a brand new `register_urls` function
+    assert hasattr(app, 'register_urls')
