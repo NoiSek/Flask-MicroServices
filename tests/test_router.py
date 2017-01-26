0a98e3686dac4c522f412faa015d86c0a54a1946
Noi Sek <noi.t.sek@gmail.com>
2017-01-25 21:18:53 -0800

    Router test coverage.

---
+++ tests/test_router.py
@@ -0,0 +1,51 @@
+import pytest
+
+
+from flask_microservices import (
+    MicroServicesApp,
+    Router,
+    url,
+    exceptions
+)
+
+
+@pytest.fixture
+def app():
+    return MicroServicesApp("testapp")
+
+
+@pytest.fixture
+def blueprint():
+    return Router.create_blueprint('home', 'module_home')
+
+
+def test_router_returns_expected(blueprint):
+    from flask import Blueprint
+
+    assert isinstance(blueprint, Blueprint)
+
+    assert blueprint.name == 'home'
+    assert blueprint._static_url_path == '/static_home'
+    assert blueprint.import_name == 'module_home'
+
+def test_router_registers_correctly(app, blueprint):
+    urlpatterns = [
+      url('/', view_func=lambda x: x, name='home'),
+      url('/test', view_func=lambda x: x, name='test')
+    ]
+
+    blueprint.register_urls(urlpatterns)
+
+    # Blueprint should queue two separate routes to be added
+    assert len(blueprint.deferred_functions) == 2
+
+    blueprint.deferred_functions[0](app)
+    blueprint.deferred_functions[1](app)
+
+    # Functions should register correctly within the app
+    assert 'home' in app.view_functions
+    assert 'test' in app.view_functions
+
+    # Rules should be added to the app's url map
+    assert list(app.url_map.iter_rules())[0].rule == '/test'
+    assert list(app.url_map.iter_rules())[1].rule == '/'
---
+++ tests/test_urls.py
@@ -0,0 +1,84 @@
+import pytest
+
+
+from flask_microservices import (
+    url,
+    exceptions
+)
+
+
+def test_url_returns_valid_simple():
+    _url = url(rule='/', view_func=lambda x: x)
+
+    # Should return a namedtuple
+    assert isinstance(_url, tuple)
+    assert hasattr(_url, '_fields')
+
+    # Should default to GET as a method
+    assert _url.methods == ['GET']
+
+    # Should not provide a name if not given one.
+    assert _url.name is None
+
+
+def test_url_returns_valid_complex():
+    # Should still return correct values when provided unnamed arguments
+    _url = url('/', lambda x: x)
+
+    assert _url.view_func(1) == 1
+    assert _url.methods == ['GET']
+    assert _url.name is None
+
+    # Should return correct values when provided a full list of unnamed arguments.
+    _url = url('/', lambda x: x, 'home', ['GET', 'POST'])
+
+    assert _url.view_func(3) == 3
+    assert _url.methods == ['GET', 'POST']
+    assert _url.name == 'home'
+
+    # Should return correct values when provided a full list of named arguments.
+    _url = url(rule='/', view_func=lambda x: x, name='_home', methods=['GET', 'POST', 'PUT'])
+
+    assert _url.view_func(5) == 5
+    assert _url.methods == ['GET', 'POST', 'PUT']
+    assert _url.name == '_home'
+
+    # Should return correct values when provided a mixture of named and unnamed arguments.
+    _url = url('/', lambda x: x, name=None, methods=['GET', 'PUT'])
+
+    assert _url.view_func(7) == 7
+    assert _url.methods == ['GET', 'PUT']
+    assert _url.name is None
+
+def test_url_returns_invalid_simple():
+    with pytest.raises(exceptions.InvalidURLPattern):
+        _url = url('', lambda x: x)
+
+    with pytest.raises(exceptions.InvalidURLPattern):
+        _url = url(10, lambda x: x)
+
+    with pytest.raises(exceptions.InvalidURLFunction):
+        _url = url('/', 'hypothetical_function_name')
+
+    with pytest.raises(exceptions.InvalidURLFunction):
+        do = lambda x: x
+        _url = url('/', do(10))
+
+def test_url_returns_invalid_complex():
+    with pytest.raises(exceptions.InvalidURLName):
+        _url = url('/', lambda x: x, name=1)
+
+    with pytest.raises(exceptions.InvalidURLName):
+        _url = url('/', lambda x: x, name='')
+
+    with pytest.raises(exceptions.UnspecifiedURLMethods):
+        _url = url('/', lambda x: x, methods=[])
+
+    with pytest.raises(exceptions.UnspecifiedURLMethods):
+        _url = url('/', lambda x: x, methods=[''])
+
+    with pytest.raises(exceptions.UnspecifiedURLMethods):
+        _url = url('/', lambda x: x, methods='GET')
+
+    with pytest.raises(exceptions.UnspecifiedURLMethods):
+        _url = url('/', lambda x: x, methods=1)
